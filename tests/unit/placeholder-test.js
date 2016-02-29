f99d6cb8d5406f72de26edc21585b54123f4c1a2
Matthew Dahl <mdahl@ciena.com>
2016-02-29 08:20:27 -0800

    get tests and linting working

---
+++ tests/unit/placeholder-test.js
@@ -0,0 +1,8 @@
+const expect = chai.expect
+import {it, describe} from 'mocha'
+
+describe('placeholder', function () {
+  it('makes tests pass', function () {
+    expect(true).to.be.truthu
+  })
+})
