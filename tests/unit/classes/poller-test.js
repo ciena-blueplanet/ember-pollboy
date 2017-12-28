d9fc419ebf61583e3f3e33c03983d75192080323
Jeremy Brown <jeremy@notmessenger.com>
2017-12-28 11:53:37 -0600

    ember install ember-frost-test

--- tests/unit/classes/poller-test.js
+++ tests/unit/classes/poller-test.js
@@ -7,11 +7,15 @@ import sinon from 'sinon'
 function createPoller (callback) {
   const context = {}
   const interval = 120
-  const poller = Poller.create({context, callback: () => {
-    callback()
-    poller.stop()
-    return RSVP.Promise.resolve(true)
-  }, interval})
+  const poller = Poller.create({
+    context,
+    callback: () => {
+      callback()
+      poller.stop()
+      return RSVP.Promise.resolve(true)
+    },
+    interval
+  })
   return poller
 }
 
@@ -20,12 +24,12 @@ function endTest (testEnd) {
 }
 
 describe('Unit : Class | Poller', function () {
-  it('polls at an interval', function (done) {
+  it('should poll at an interval', function (done) {
     const poller = createPoller(done)
     poller.start()
   })
 
-  it('can stop polling', function (done) {
+  it('should be able to stop polling', function (done) {
     const spy = sinon.spy()
     const poller = createPoller(spy)
     poller.start()
@@ -35,7 +39,7 @@ describe('Unit : Class | Poller', function () {
     })
   })
 
-  it('can be paused', function (done) {
+  it('should be able to be paused', function (done) {
     const spy = sinon.spy()
     const poller = createPoller(spy)
     poller.start()
@@ -47,7 +51,7 @@ describe('Unit : Class | Poller', function () {
     })
   })
 
-  it('can resume from being paused', function (done) {
+  it('should be able to resume from being paused', function (done) {
     const spy = sinon.spy()
     const poller = createPoller(spy)
     poller.start()
